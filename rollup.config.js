8a4399996b4e77da2f2f50d82192011d459669e3
Zhanghao <yukiniro@hotmail.com>
2022-04-01 23:21:30 +0800

    feat: init commit

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import { babel } from "@rollup/plugin-babel";
+import fileSize from "rollup-plugin-filesize";
+import typescript from "rollup-plugin-typescript2";
+import { readFile } from "fs/promises";
+
+function getConfig(libraryName) {
+  return {
+    input: "src/index.ts",
+    output: [
+      {
+        file: `./dist/${libraryName}.cjs.js`,
+        format: "cjs",
+      },
+      {
+        file: `./dist/${libraryName}.esm.js`,
+        format: "esm",
+      },
+    ],
+    plugins: [
+      typescript(),
+      babel({
+        babelHelpers: "bundled",
+        presets: ["@babel/preset-env"],
+      }),
+      fileSize(),
+    ],
+  };
+}
+
+export default readFile("./package.json", "utf8")
+  .then((json) => {
+    const libraryName = JSON.parse(json).name;
+    return getConfig(libraryName);
+  })
+  .catch((e) => {
+    console.log(e);
+    return getConfig("my-project");
+  });
